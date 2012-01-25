d41ee0da6c0ff06e1a47cf90c12cb3d97a949b03
Jeremy B. Merrill <jeremy@jeremybmerrill.com>
2012-01-24 19:50:07 -0800

    Added root_url fallback if twitterhandle is invalid.

--- twittersubmit.php
+++ twittersubmit.php
@@ -4,6 +4,7 @@ require_once('bingoconfig.php');
 
 global $consumerSecret;
 global $oAuthSecret;
+global $root_url;
 
 $consumerKey    = '8jp7xABzIsLQkZ07OzImA';
 $oAuthToken     = '378594590-uU9igS21ORUtPeeJiQixlKu6k3gcflOA4DSCHAxf';
@@ -19,7 +20,13 @@ $tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecr
 
 //send a tweet
 $status = $username . ' just got BINGO at #ONA11 #BuzzwordBingo while at ' . $hashtag . '. Play along at http://ona11.journalists.org/bingo';
-$tweet->post('statuses/update', array('status' => $status));
-
-header('Location: http://twitter.com');
+if ($username != "@twitterhandle" && $username != ""){
+        $tweet->post('statuses/update', array('status' => $status));
+        header('Location: http://twitter.com');
+}else{
+        if( substr($root_url, -1) != "/"){
+            $root_url = $root_url . "/"
+        }
+        header('Location: '+ $root_url + 'yougotbingo.php');
+}
 ?>
